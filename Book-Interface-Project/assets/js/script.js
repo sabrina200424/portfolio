4a189ac56757cdc64077628f8d849443be11c27a
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2026-04-24 23:58:18 +0000

    Added modifications to book interface

--- Book-Interface-Project/assets/js/script.js
+++ Book-Interface-Project/assets/js/script.js
@@ -214,6 +214,19 @@ async function ShowResults(){
             let AuthorImageTwo = "https://covers.openlibrary.org/a/olid/" + author_image_two + "-M.jpg"
             let AuthorImageThree = "https://covers.openlibrary.org/a/olid/" + author_image_three + "-M.jpg"
 
+            let key = msg1.docs[0].key;
+            let key_two = msg1.docs[1].key;
+            let key_three = msg1.docs[2].key;
+
+
+            let information = "https://openlibrary.org" + key + ".json"
+            let msg2Object = await fetch(information); 
+
+            let information_two = "https://openlibrary.org" + key_two + ".json"
+            let msg3Object = await fetch(information_two);
+
+            let information_three = "https://openlibrary.org" + key_three + ".json"
+            let msg4Object = await fetch(information_three);
 
             // let authorlist = "";
             // for (let i = 0; i < msg1.docs[0].author_name.length; i++) {
@@ -235,11 +248,63 @@ async function ShowResults(){
             document.getElementById("RetrievedImageTwo").src = AuthorImageTwo;
             document.getElementById("RetrievedImageThree").src = AuthorImageThree;
 
-           
+           if (summary_type_one = "object"){
+                 let summary = msg2.description.value;
+                document.getElementById("Summary").innerHTML = "Summary: " + summary;
+             }
+
+            else if (summary_type_one = "string") {
+                  let summary = msg2.description; 
+                    document.getElementById("Summary").innerHTML = "Summary: " + summary; 
+                }
+
+             else if (summary_type_one = undefined){
+              document.getElementById("Summary").innerHTML = "Summary not found."; 
+              }
+
+
+            if (summary_type_two = "object"){
+                 let summary_two = msg3.description.value;
+                document.getElementById("SummaryTwo").innerHTML = "Summary: " + summary_two;
+             }
+
+            else if (summary_type_two = "string") {
+                  let summary_two = msg3.description; 
+                    document.getElementById("SummaryTwo").innerHTML = "Summary: " + summary_two; 
+                }
+
+             else if (summary_type_two = undefined){
+              document.getElementById("SummaryTwo").innerHTML = "Summary not found."; 
+              }
+            
+
+
+            if (summary_type_three = "object"){
+                 let summary_three = msg4.description.value;
+                document.getElementById("SummaryThree").innerHTML = "Summary: " + summary_three;
+             }
+
+            else if (summary_type_three = "string") {
+                  let summary_three = msg4.description; 
+                    document.getElementById("SummaryThree").innerHTML = "Summary: " + summary_three; 
+                }
+
+             else if (summary_type_three = undefined){
+              document.getElementById("SummaryThree").innerHTML = "Summary not found."; 
+              }
+            
 
             
 
         }
+
+        else {
+            if (msg1Object.status != 200 || msg1Object.status < 200 || msg1Object.status != 299 || msg1Object.status > 299) {
+            alert("The data you are looking for does not exist." + msg1Object.status)
+            return;
+            }
+        }
+
         
     }
 
@@ -260,32 +325,109 @@ async function ShowResults(){
             let author_name_three = msg1.docs[2].author_name[0]; 
             let title = msg1.docs[0].title;
             let titletwo = msg1.docs[1].title;  
+            let titlethree = msg1.docs[2].title;
             let first_publish_year = msg1.docs[0].first_publish_year;
             let first_publish_year_two = msg1.docs[1].first_publish_year;
+            let first_publish_year_three = msg1.docs[2].first_publish_year;
 
             let cover_edition_key = msg1.docs[0].cover_edition_key;
-            let cover_edition_key_two = msg1.docs[1].cover_edition_key; 
+            let cover_edition_key_two = msg1.docs[1].cover_edition_key;
+            let cover_edition_key_three = msg1.docs[2].cover_edition_key; 
 
             let CoverImage = "https://covers.openlibrary.org/b/olid/" + cover_edition_key + "-L.jpg"
             let CoverImageTwo = "https://covers.openlibrary.org/b/olid/" + cover_edition_key_two + "-L.jpg"
+            let CoverImageThree = "https://covers.openlibrary.org/b/olid/" + cover_edition_key_three + "-L.jpg"
 
             // let authorlist = "";
             // for (let i = 0; i < msg1.docs[0].author_name.length; i++) {
             //     authorlist += msg1.docs[0].author_name[i] + "<br>";
             // }
+
+            let key = msg1.docs[0].key;
+            let key_two = msg1.docs[1].key;
+            let key_three = msg1.docs[2].key;
+
+
+            let information = "https://openlibrary.org" + key + ".json"
+            let msg2Object = await fetch(information); 
+
+            let information_two = "https://openlibrary.org" + key_two + ".json"
+            let msg3Object = await fetch(information_two);
+
+            let information_three = "https://openlibrary.org" + key_three + ".json"
+            let msg4Object = await fetch(information_three);
+
+
+
             document.getElementById("Author").innerHTML = "First author's name: " + author_name_one;
             document.getElementById("AuthorTwo").innerHTML = "Second author's name: " + author_name_two;
             document.getElementById("AuthorThree").innerHTML = "Third author's name: " + author_name_three;
             document.getElementById("Title").innerHTML = "Book Title: " + title;
             document.getElementById("TitleTwo").innerHTML = "Second Book Title: " + titletwo;
+            document.getElementById("TitleThree").innerHTML = "Third Book Title: " + titlethree;
             document.getElementById("PublishedYear").innerHTML = "First Published Year: " + first_publish_year;
             document.getElementById("PublishedYearTwo").innerHTML = "First Published Year: " + first_publish_year_two;
+            document.getElementById("PublishedYearThree").innerHTML = "First Published Year: " + first_publish_year_three;
             document.getElementById("RetrievedImage").src = CoverImage;
             document.getElementById("RetrievedImageTwo").src = CoverImageTwo;
+            document.getElementById("RetrievedImageThree").src = CoverImageThree;
 
+
+            if (summary_type_one = "object"){
+                 let summary = msg2.description.value;
+                document.getElementById("Summary").innerHTML = "Summary: " + summary;
+             }
+
+            else if (summary_type_one = "string") {
+                  let summary = msg2.description; 
+                    document.getElementById("Summary").innerHTML = "Summary: " + summary; 
+                }
+
+             else if (summary_type_one = undefined){
+              document.getElementById("Summary").innerHTML = "Summary not found."; 
+              }
+
+
+            if (summary_type_two = "object"){
+                 let summary_two = msg3.description.value;
+                document.getElementById("SummaryTwo").innerHTML = "Summary: " + summary_two;
+             }
+
+            else if (summary_type_two = "string") {
+                  let summary_two = msg3.description; 
+                    document.getElementById("SummaryTwo").innerHTML = "Summary: " + summary_two; 
+                }
+
+             else if (summary_type_two = undefined){
+              document.getElementById("SummaryTwo").innerHTML = "Summary not found."; 
+              }
+            
+
+
+            if (summary_type_three = "object"){
+                 let summary_three = msg4.description.value;
+                document.getElementById("SummaryThree").innerHTML = "Summary: " + summary_three;
+             }
+
+            else if (summary_type_three = "string") {
+                  let summary_three = msg4.description; 
+                    document.getElementById("SummaryThree").innerHTML = "Summary: " + summary_three; 
+                }
+
+             else if (summary_type_three = undefined){
+              document.getElementById("SummaryThree").innerHTML = "Summary not found."; 
+              }
             
 
         }
+
+        else {
+            if (msg1Object.status != 200 || msg1Object.status < 200 || msg1Object.status != 299 || msg1Object.status > 299) {
+            alert("The data you are looking for does not exist." + msg1Object.status)
+            return;
+            }
+        }
+
     }
 
     if(textCount > 1)
